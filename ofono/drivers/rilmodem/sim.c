ddaa2288147b87c91a7172fd38dde1b2036d295c
Jussi Kangas <jussi.kangas@tieto.com>
2013-06-11 15:56:13 +0300

    [rilmodem][WIP] enabling pin and pin2 code changing
    Enables basic functionality for PIN and PIN2 code changing
    in Rilmodem.
    
    Signed-off-by: jussi.kangas@tieto.com

--- ofono/drivers/rilmodem/sim.c
+++ ofono/drivers/rilmodem/sim.c
@@ -754,6 +754,47 @@ static void ril_pin_send_puk(struct ofono_sim *sim,
 	}
 }
 
+static void ril_change_passwd(struct ofono_sim *sim,
+				enum ofono_sim_password_type passwd_type,
+				const char *old, const char *new,
+				ofono_sim_lock_unlock_cb_t cb, void *data)
+{
+	struct sim_data *sd = ofono_sim_get_data(sim);
+	struct cb_data *cbd = cb_data_new(cb, data);
+	struct parcel rilp;
+	char oldpass[0x0A + 1];
+	char newpass[0x0A + 1];
+	int ret = 0;
+	int id = RIL_REQUEST_CHANGE_SIM_PIN;
+
+	parcel_init(&rilp);
+
+	parcel_w_int32(&rilp, 3);			/* Number of params */
+
+	snprintf(oldpass, sizeof(oldpass), "%s", old);
+
+	parcel_w_string(&rilp, oldpass);	/* puk */
+
+	snprintf(newpass, sizeof(newpass), "%s", new);
+
+	parcel_w_string(&rilp, newpass);	/* passwd */
+
+	parcel_w_string(&rilp, sd->app_id);	/* AID (Application ID) */
+
+	if (passwd_type == OFONO_SIM_PASSWORD_SIM_PIN2)
+		id = RIL_REQUEST_CHANGE_SIM_PIN2;
+
+	ret = g_ril_send(sd->ril, id, rilp.data, rilp.size,
+			ril_pin_change_state_cb, cbd, g_free);
+
+	parcel_free(&rilp);
+
+	if (ret <= 0) {
+		g_free(cbd);
+		CALLBACK_WITH_FAILURE(cb, data);
+	}
+}
+
 static void ril_sim_notify(struct ril_msg *message, gpointer user_data)
 {
 	struct ofono_sim *sim = user_data;
@@ -833,6 +874,7 @@ static struct ofono_sim_driver driver = {
 	.send_passwd		= ril_pin_send,
 	.lock				= ril_pin_change_state,
 	.reset_passwd		= ril_pin_send_puk,
+	.change_passwd		= ril_change_passwd,
 
 /*
  * TODO: Implmenting PIN/PUK support requires defining
@@ -844,7 +886,6 @@ static struct ofono_sim_driver driver = {
  * function sim_initialize_after_pin() is called.
  *
  *	.query_pin_retries	= ril_pin_retries_query,
- *	.change_passwd		= ril_change_passwd,
  *	.query_locked		= ril_pin_query_enabled,
  *
  * TODO: Implementing SIM write file IO support requires
