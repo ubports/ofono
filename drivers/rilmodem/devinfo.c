a5509dfcc0e24e413de999379ef827d018c716e7
Petri Takalokastari <petri.takalokastari@oss.tieto.com>
2013-07-04 10:53:59 +0300

    [PATCH] [rilmodem][WIP] Add IMEI and revision query support
    This is based on code from Canonical
    https://code.launchpad.net/~phablet-team/phablet-extras/ofono
    
    
    Signed-off-by: Petri Takalokastari <petri.takalokastari@oss.tieto.com>

--- drivers/rilmodem/devinfo.c
+++ drivers/rilmodem/devinfo.c
@@ -4,6 +4,7 @@
  *
  *  Copyright (C) 2008-2011  Intel Corporation. All rights reserved.
  *  Copyright (C) 2012 Canonical Ltd.
+ *	Copyright (C) 2013 Jolla Ltd.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
@@ -76,34 +77,91 @@ static void ril_query_model(struct ofono_devinfo *info,
 	g_free(cbd);
 }
 
+static void query_revision_cb(struct ril_msg *message, gpointer user_data)
+{
+	struct cb_data *cbd = user_data;
+	ofono_devinfo_query_cb_t cb = cbd->cb;
+	struct ofono_error error;
+	struct parcel rilp;
+	gchar *revision;
+
+	if (message->error == RIL_E_SUCCESS) {
+		decode_ril_error(&error, "OK");
+	} else {
+		decode_ril_error(&error, "FAIL");
+		cb(&error, NULL, cbd->data);
+		return;
+	}
+
+	ril_util_init_parcel(message, &rilp);
+	revision = parcel_r_string(&rilp);
+
+	cb(&error, revision, cbd->data);
+
+	g_free(revision);
+}
+
 static void ril_query_revision(struct ofono_devinfo *info,
 				ofono_devinfo_query_cb_t cb,
 				void *data)
 {
-	const char *attr = "Fake Revision";
 	struct cb_data *cbd = cb_data_new(cb, data);
+	GRil *ril = ofono_devinfo_get_data(info);
+	int request = RIL_REQUEST_BASEBAND_VERSION;
+	int ret;
+
+	ret = g_ril_send(ril, request, NULL, 0,
+					query_revision_cb, cbd, g_free);
+
+	if (ret <= 0) {
+		g_free(cbd);
+		CALLBACK_WITH_FAILURE(cb, NULL, data);
+	}
+}
+
+static void query_serial_cb(struct ril_msg *message, gpointer user_data)
+{
+	struct cb_data *cbd = user_data;
+	ofono_devinfo_query_cb_t cb = cbd->cb;
 	struct ofono_error error;
-	decode_ril_error(&error, "OK");
+	struct parcel rilp;
+	gchar *imei;
 
-	cb(&error, attr, cbd->data);
+	if (message->error == RIL_E_SUCCESS) {
+		decode_ril_error(&error, "OK");
+	} else {
+		decode_ril_error(&error, "FAIL");
+		cb(&error, NULL, cbd->data);
+		return;
+	}
 
-	/* Note: this will need to change if cbd passed to gril layer */
-	g_free(cbd);
+	ril_util_init_parcel(message, &rilp);
+
+	imei = parcel_r_string(&rilp);
+
+	cb(&error, imei, cbd->data);
+
+	g_free(imei);
 }
 
 static void ril_query_serial(struct ofono_devinfo *info,
 				ofono_devinfo_query_cb_t cb,
 				void *data)
 {
-	const char *attr = "THIS-IS-A-FAKE-SERIAL-NO";
 	struct cb_data *cbd = cb_data_new(cb, data);
-	struct ofono_error error;
-	decode_ril_error(&error, "OK");
+	GRil *ril = ofono_devinfo_get_data(info);
+	/* TODO: make it support both RIL_REQUEST_GET_IMEI (deprecated) and
+	 * RIL_REQUEST_DEVICE_IDENTITY depending on the rild version used */
+	int request = RIL_REQUEST_GET_IMEI;
+	int ret;
 
-	cb(&error, attr, cbd->data);
+	ret = g_ril_send(ril, request, NULL, 0,
+					query_serial_cb, cbd, g_free);
 
-	/* Note: this will need to change if cbd passed to gril layer */
-	g_free(cbd);
+	if (ret <= 0) {
+		g_free(cbd);
+		CALLBACK_WITH_FAILURE(cb, NULL, data);
+	}
 }
 
 static gboolean ril_delayed_register(gpointer user_data)
@@ -126,9 +184,7 @@ static int ril_devinfo_probe(struct ofono_devinfo *info, unsigned int vendor,
 
 	ofono_devinfo_set_data(info, ril);
 
-	DBG("");
-
-        /*
+	/*
 	 * TODO: analyze if capability check is needed
 	 * and/or timer should be adjusted.
 	 *
@@ -138,7 +194,7 @@ static int ril_devinfo_probe(struct ofono_devinfo *info, unsigned int vendor,
 	 * some kind of capabilities query to the modem, and then
 	 * call register in the callback; we use a timer instead.
 	 */
-        g_timeout_add_seconds(1, ril_delayed_register, info);
+	g_timeout_add_seconds(1, ril_delayed_register, info);
 
 	return 0;
 }
