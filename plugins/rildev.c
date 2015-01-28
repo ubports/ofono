36a7d86e175e999dbeeb4b6fda75c1c1656af74d
Jarko Poutiainen <jarko.poutiainen@oss.tieto.com>
2015-01-28 14:29:16 +0200

    [plugins] rildev to detect rild socket
    Signed-off-by: Jarko Poutiainen <jarko.poutiainen@oss.tieto.com>

--- plugins/rildev.c
+++ plugins/rildev.c
@@ -4,6 +4,7 @@
  *
  *  Copyright (C) 2008-2011  Intel Corporation. All rights reserved.
  *  Copyright (C) 2014 Canonical Ltd.
+ *  Copyright (C) 2015 Jolla Ltd.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
@@ -30,27 +31,91 @@
 
 #include <glib.h>
 #include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/inotify.h>
 
 #define OFONO_API_SUBJECT_TO_CHANGE
 #include <ofono/plugin.h>
 #include <ofono/modem.h>
 #include <ofono/log.h>
 
+#include "rildev.h"
+
+static int inotify_fd = -1;
+static int inotify_watch_id = -1;
+static guint inotify_watch_source_id;
+static GIOChannel *inotify_watch_channel;
+
 static GSList *modem_list;
+static int watch_for_rild_socket(void);
+static void detect_rild(void);
 
-static int detect_init(void)
+static struct ofono_modem *find_ril_modem(int slot)
 {
-	int retval;
+	GSList *list;
+
+	for (list = modem_list; list; list = list->next) {
+		struct ofono_modem *modem = list->data;
+		int ril_slot = ofono_modem_get_integer(modem, "Slot");
+
+		if (ril_slot == slot)
+			return modem;
+	}
+
+	return NULL;
+}
+
+static void remove_watchers(void)
+{
+	if (inotify_watch_source_id == 0)
+		return;
+
+	g_source_remove(inotify_watch_source_id);
+	inotify_watch_source_id = 0;
+	g_io_channel_unref(inotify_watch_channel);
+	inotify_watch_channel = NULL;
+	inotify_rm_watch(inotify_fd, inotify_watch_id);
+	inotify_watch_id = -1;
+	close(inotify_fd);
+	inotify_fd = -1;
+}
+
+void ril_modem_remove(struct ofono_modem *modem)
+{
+	struct ofono_modem *list_modem;
+	int slot = -1;
+	list_modem = NULL;
+
+	if (modem)
+		slot = ofono_modem_get_integer(modem, "Slot");
+
+	if (slot >= 0)
+		list_modem = find_ril_modem(slot);
+
+	if (list_modem) {
+		ofono_modem_remove(modem);
+		modem_list = g_slist_remove(modem_list, list_modem);
+	}
+
+	detect_rild();
+
+}
+
+static int create_rilmodem(const char *ril_type, int slot)
+{
 	struct ofono_modem *modem;
-	const char *ril_type;
+	char dev_name[64];
+	int retval;
+	DBG("");
+	snprintf(dev_name, sizeof(dev_name), "ril_%d", slot);
 
-	if ((ril_type = getenv("OFONO_RIL_DEVICE")) == NULL)
-		ril_type = "ril";
-
-	ofono_info("RILDEV Detected modem type %s", ril_type);
+	/* Check that not created already */
+	if (find_ril_modem(slot))
+		return 0;
 
 	/* Currently there is only one ril implementation, create always */
-	modem = ofono_modem_create("ril_0", ril_type);
+	modem = ofono_modem_create(dev_name, ril_type);
 	if (modem == NULL) {
 		DBG("ofono_modem_create failed for type %s", ril_type);
 		return -ENODEV;
@@ -58,9 +123,14 @@ static int detect_init(void)
 
 	modem_list = g_slist_prepend(modem_list, modem);
 
+	ofono_modem_set_integer(modem, "Slot", slot);
+
 	/* This causes driver->probe() to be called... */
-	retval = ofono_modem_register(modem);
-	DBG("ofono_modem_register returned: %d", retval);
+	if ((retval = ofono_modem_register(modem)) != 0) {
+		ofono_error("%s: ofono_modem_register returned: %d",
+				__func__, retval);
+		return retval;
+	}
 
 	/*
 	 * kickstart the modem:
@@ -81,9 +151,68 @@ static int detect_init(void)
 	return 0;
 }
 
+static gboolean rild_inotify(GIOChannel *gio, GIOCondition c, gpointer data)
+{
+	DBG("");
+
+	if (access(RILD_CMD_SOCKET, F_OK) != -1){
+		create_rilmodem("ril", 0);
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+static int watch_for_rild_socket(void)
+{
+	inotify_fd = inotify_init();
+	if (inotify_fd < 0)
+		return -EIO;
+
+	inotify_watch_channel = g_io_channel_unix_new(inotify_fd);
+	if (inotify_watch_channel == NULL)
+		return -EIO;
+
+	/* No rild socket found so let's wait and see if one appears */
+	/* Note: We could also use IN_DELETE to follow if rild dissappears */
+	inotify_watch_id = inotify_add_watch(inotify_fd,
+					RILD_SOCKET_DIR,
+					IN_CREATE);
+
+	/* Add more handling here */
+	if (inotify_watch_id < 0)
+		return -EIO;
+
+	inotify_watch_source_id = g_io_add_watch(inotify_watch_channel, G_IO_IN,
+							rild_inotify, NULL);
+	if (inotify_watch_source_id <= 0)
+		return -EIO;
+
+	return 0;
+}
+
+static void detect_rild(void)
+{
+	if (rild_inotify(NULL,0,NULL))
+		watch_for_rild_socket();
+
+	/* Let's recheck if we just missed the rild */
+	if (!rild_inotify(NULL,0,NULL))
+		remove_watchers();
+}
+
+static int detect_init(void)
+{
+	DBG("");
+	detect_rild();
+	return 0;
+}
+
 static void detect_exit(void)
 {
 	GSList *list;
+
+	DBG("");
 
 	for (list = modem_list; list; list = list->next) {
 		struct ofono_modem *modem = list->data;
@@ -93,6 +222,7 @@ static void detect_exit(void)
 
 	g_slist_free(modem_list);
 	modem_list = NULL;
+	remove_watchers();
 }
 
 OFONO_PLUGIN_DEFINE(rildev, "ril type detection", VERSION,
