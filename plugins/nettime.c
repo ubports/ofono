bd1d243550d5983c7b03e763abc1a8612f9f6d5d
Slava Monich <slava.monich@jolla.com>
2015-09-08 18:50:16 +0300

    [ofono] Updated nettime plugin to support more than one modem. MER#1110

--- plugins/nettime.c
+++ plugins/nettime.c
@@ -2,7 +2,7 @@
  *
  *  oFono - Open Source Telephony
  *
- *  Copyright (C) 2012-2013  Jolla Ltd.
+ *  Copyright (C) 2012-2015  Jolla Ltd.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
@@ -32,11 +32,8 @@
 #include <ofono/plugin.h>
 #include <ofono/log.h>
 #include <ofono/nettime.h>
-#include <ofono/types.h>
 
 #include "ofono.h"
-
-#include "common.h"
 
 struct nt_data {
 	gboolean time_available;
@@ -48,26 +45,32 @@ struct nt_data {
 	int dst;
 	int time_zone;
 
-	const char *mcc;
-	const char *mnc;
-	const char *path;
+	char *mcc;
+	char *mnc;
+	char* path;
+	DBusConnection *conn;
 };
 
-static struct nt_data *ntd = NULL;
-
-
-static void init_time(void)
+static struct nt_data *nettime_new(const char *path)
 {
-	ntd = g_new0(struct nt_data, 1);
+	struct nt_data *ntd = g_new0(struct nt_data, 1);
 
-	ntd->time_available = FALSE;
-	ntd->time_pending = FALSE;
-	ntd->dst = 0;
-	ntd->time_zone = 0;
+	ntd->path = g_strdup(path);
+	ntd->conn = dbus_connection_ref(ofono_dbus_get_connection());
+	return ntd;
 }
 
-static gboolean encode_time_format(const struct ofono_network_time *time,
-				 struct tm *tm)
+static void nettime_free(struct nt_data *ntd)
+{
+	dbus_connection_unref(ntd->conn);
+	g_free(ntd->path);
+	g_free(ntd->mcc);
+	g_free(ntd->mnc);
+	g_free(ntd);
+}
+
+static gboolean nettime_encode_time_format(struct tm *tm,
+					const struct ofono_network_time *time)
 {
 	if (time->year < 0)
 		return FALSE;
@@ -85,7 +88,7 @@ static gboolean encode_time_format(const struct ofono_network_time *time,
 	return TRUE;
 }
 
-static time_t get_monotonic_time()
+static time_t nettime_get_monotonic_time()
 {
 	struct timespec ts;
 	memset(&ts, 0, sizeof(struct timespec));
@@ -98,8 +101,7 @@ static time_t get_monotonic_time()
 	return ts.tv_sec;
 }
 
-static int fill_time_notification(DBusMessage *msg,
-					struct nt_data *ntd)
+static int nettime_fill_time_notification(DBusMessage *msg, struct nt_data *ntd)
 {
 	DBusMessageIter iter, iter_array;
 	dbus_int64_t utc_long, received;
@@ -150,22 +152,22 @@ static int fill_time_notification(DBusMessage *msg,
 	return 0;
 }
 
-static DBusMessage *get_network_time(DBusConnection *conn,
+static DBusMessage *nettime_get_network_time(DBusConnection *conn,
 						DBusMessage *msg, void *data)
 {
-	DBusMessage *reply;
+	struct nt_data *ntd = data;
+	DBusMessage *reply = dbus_message_new_method_return(msg);
 
-	reply = dbus_message_new_method_return(msg);
-	if (reply == NULL)
-		return NULL;
-	fill_time_notification(reply, ntd);
+	if (reply) {
+		nettime_fill_time_notification(reply, ntd);
+	}
 	return reply;
 }
 
 static const GDBusMethodTable nettime_methods[] = {
 	{ GDBUS_METHOD("GetNetworkTime",
 			NULL, GDBUS_ARGS({ "time", "a{sv}" }),
-			get_network_time) },
+			nettime_get_network_time) },
 	{ }
 };
 
@@ -177,84 +179,90 @@ static const GDBusSignalTable nettime_signals[] = {
 
 static int nettime_probe(struct ofono_nettime_context *context)
 {
-	DBusConnection *conn = ofono_dbus_get_connection();
-	const char *path = ofono_modem_get_path(context->modem);
-	DBG("Network time probe for modem: %p (%s)", context->modem, path);
-	init_time();
-	if (!g_dbus_register_interface(conn, path,
-					OFONO_NETWORK_TIME_INTERFACE, // name
-					nettime_methods, // methods
-					nettime_signals, // signals
-					NULL, // GDBusPropertyTable *properties
-					NULL, // user data
-					NULL)) { // GDBusDestroyFunction destroy
-		ofono_error("Network time: Could not register interface %s, path %s",
-				OFONO_NETWORK_TIME_INTERFACE, path);
-		return 1;
-	} else {
-		ofono_info("Network time: Registered interface %s, path %s",
-				OFONO_NETWORK_TIME_INTERFACE, path);
-	}
+	struct nt_data *ntd = nettime_new(ofono_modem_get_path(context->modem));
 
-	ofono_modem_add_interface(context->modem, OFONO_NETWORK_TIME_INTERFACE);
-	return 0;
+	DBG("Network time probe for modem: %p (%s)", context->modem, ntd->path);
+	if (g_dbus_register_interface(ntd->conn, ntd->path,
+			OFONO_NETWORK_TIME_INTERFACE, nettime_methods,
+			nettime_signals, NULL, ntd, NULL)) {
+		context->data = ntd;
+		ofono_info("Registered interface %s, path %s",
+				OFONO_NETWORK_TIME_INTERFACE, ntd->path);
+		ofono_modem_add_interface(context->modem,
+					OFONO_NETWORK_TIME_INTERFACE);
+		return 0;
+	} else {
+		ofono_error("Could not register interface %s, path %s",
+				OFONO_NETWORK_TIME_INTERFACE, ntd->path);
+		nettime_free(ntd);
+		return 1;
+	}
 }
 
 static void nettime_remove(struct ofono_nettime_context *context)
 {
-	DBusConnection *conn = ofono_dbus_get_connection();
-	const char *path = ofono_modem_get_path(context->modem);
-	DBG("Network time remove for modem: %p (%s)", context->modem, path);
-	if (!g_dbus_unregister_interface(conn, path, OFONO_NETWORK_TIME_INTERFACE)) {
-		ofono_error("Network time: could not unregister interface %s, path %s",
-				OFONO_NETWORK_TIME_INTERFACE, path);
+	struct nt_data *ntd = context->data;
+
+	DBG("Network time remove for modem: %p (%s)", context->modem,
+					ofono_modem_get_path(context->modem));
+	ofono_modem_remove_interface(context->modem,
+					OFONO_NETWORK_TIME_INTERFACE);
+	if (!g_dbus_unregister_interface(ntd->conn, ntd->path,
+					OFONO_NETWORK_TIME_INTERFACE)) {
+		ofono_error("Network time: could not unregister interface %s"
+			" for %s", OFONO_NETWORK_TIME_INTERFACE, ntd->path);
 	}
 
-	ofono_modem_remove_interface(context->modem, OFONO_NETWORK_TIME_INTERFACE);
-	g_free(ntd);
+	nettime_free(ntd);
 }
 
-static void send_signal(struct nt_data *ntd)
+static void nettime_send_signal(struct nt_data *ntd)
 {
-	DBusConnection *conn = ofono_dbus_get_connection();
 	DBusMessage *signal = dbus_message_new_signal(ntd->path,
-							OFONO_NETWORK_TIME_INTERFACE,
+						OFONO_NETWORK_TIME_INTERFACE,
 							"NetworkTimeChanged");
-	fill_time_notification(signal, ntd);
-	g_dbus_send_message(conn, signal);
+
+	nettime_fill_time_notification(signal, ntd);
+	g_dbus_send_message(ntd->conn, signal);
 }
 
 static void nettime_info_received(struct ofono_nettime_context *context,
 					struct ofono_network_time *info)
 {
+	struct nt_data *ntd = context->data;
 	struct ofono_netreg *netreg;
+	const char *mcc;
+	const char *mnc;
 	struct tm t;
 
-	if (info == NULL)
+	if (!ntd)
 		return;
 
 	netreg = __ofono_atom_get_data(__ofono_modem_find_atom(
-						context->modem, OFONO_ATOM_TYPE_NETREG));
+				context->modem, OFONO_ATOM_TYPE_NETREG));
+	mcc = ofono_netreg_get_mcc(netreg);
+	mnc = ofono_netreg_get_mnc(netreg);
 
-	if (!(ofono_netreg_get_mcc(netreg) && ofono_netreg_get_mnc(netreg))) {
+	if (!mcc || !mnc) {
 		DBG("Incomplete network time received, ignoring");
 		return;
 	}
 
-	ntd->path = ofono_modem_get_path(context->modem);
-	ntd->mcc = ofono_netreg_get_mcc(netreg);
-	ntd->mnc = ofono_netreg_get_mnc(netreg);
-	ntd->received = get_monotonic_time();
+	g_free(ntd->mcc);
+	g_free(ntd->mnc);
+	ntd->mcc = g_strdup(mcc);
+	ntd->mnc = g_strdup(mnc);
+	ntd->received = nettime_get_monotonic_time();
 	ntd->time_pending = TRUE;
 	ntd->dst = info->dst;
 	ntd->time_zone = info->utcoff;
-
-	ntd->time_available = encode_time_format(info, &t);
-	if (ntd->time_available == TRUE)
+	ntd->time_available = nettime_encode_time_format(&t, info);
+	if (ntd->time_available) {
 		ntd->nw_time_utc = timegm(&t);
+	}
 
-	send_signal(ntd);
-	DBG("modem: %p (%s)", context->modem, ofono_modem_get_path(context->modem));
+	nettime_send_signal(ntd);
+	DBG("modem: %p (%s)", context->modem, ntd->path);
 	DBG("time: %04d-%02d-%02d %02d:%02d:%02d%c%02d:%02d (DST=%d)",
 		info->year, info->mon, info->mday, info->hour,
 		info->min, info->sec, info->utcoff > 0 ? '+' : '-',
