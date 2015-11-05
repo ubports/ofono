19166a33efd284f7416f13054c845e9c379c549c
Denis Kenzior <denkenz@gmail.com>
2015-11-05 13:50:40 -0600

    gril: Remove g_ril_request_send_ussd

--- ofono/gril/grilrequest.h
+++ ofono/gril/grilrequest.h
@@ -208,10 +208,6 @@ void g_ril_request_separate_conn(GRil *gril,
 void g_ril_request_set_supp_svc_notif(GRil *gril,
 					struct parcel *rilp);
 
-void g_ril_request_send_ussd(GRil *gril,
-				const char *ussd,
-				struct parcel *rilp);
-
 void g_ril_request_screen_state(GRil *gril,
 				int state,
 				struct parcel *rilp);
