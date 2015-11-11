b5be8420abb4cd2f12e9278016a61fc5bb78b0c9
Slava Monich <slava.monich@jolla.com>
2015-11-11 17:14:32 +0200

    [ril] Add GetPresentSims to ModemManager interface. Contributes to JB#33265
    Only a simple boolean (presence/absence) is reported. PresentSimsChanged
    signal is emitted when SIM is inserted or removed (if RIL supports it).

--- ofono/drivers/ril/ril_plugin_dbus.c
+++ ofono/drivers/ril/ril_plugin_dbus.c
@@ -31,28 +31,66 @@ struct ril_plugin_dbus {
 
 #define RIL_DBUS_PATH               "/"
 #define RIL_DBUS_INTERFACE          "org.nemomobile.ofono.ModemManager"
-#define RIL_DBUS_INTERFACE_VERSION  (1)
+#define RIL_DBUS_INTERFACE_VERSION  (2)
 
-#define RIL_DBUS_ENABLED_MODEM_CHANGED_SIGNAL       "EnabledModemsChanged"
+#define RIL_DBUS_ENABLED_MODEMS_CHANGED_SIGNAL      "EnabledModemsChanged"
+#define RIL_DBUS_PRESENT_SIMS_CHANGED_SIGNAL        "PresentSimsChanged"
 #define RIL_DBUS_DEFAULT_VOICE_SIM_CHANGED_SIGNAL   "DefaultVoiceSimChanged"
 #define RIL_DBUS_DEFAULT_DATA_SIM_CHANGED_SIGNAL    "DefaultDataSimChanged"
 #define RIL_DBUS_DEFAULT_VOICE_MODEM_CHANGED_SIGNAL "DefaultVoiceModemChanged"
 #define RIL_DBUS_DEFAULT_DATA_MODEM_CHANGED_SIGNAL  "DefaultDataModemChanged"
 #define RIL_DBUS_IMSI_AUTO                          "auto"
 
-static void ril_plugin_dbus_append_path_array(DBusMessageIter *it, char **paths)
+typedef gboolean
+(*ril_plugin_dbus_slot_select_fn) (const struct ril_slot_info *);
+typedef const char *
+(*ril_plugin_dbus_slot_string_fn) (const struct ril_slot_info *);
+
+static gboolean ril_plugin_dbus_enabled(const struct ril_slot_info *slot)
+{
+	return slot->enabled;
+}
+
+static gboolean ril_plugin_dbus_present(const struct ril_slot_info *slot)
+{
+	return slot->sim_present;
+}
+
+static void ril_plugin_dbus_append_path_array(DBusMessageIter *it,
+	struct ril_plugin_dbus *dbus, ril_plugin_dbus_slot_select_fn selector)
 {
 	DBusMessageIter array;
+	const struct ril_slot_info *const *ptr = dbus->plugin->slots;
 
 	dbus_message_iter_open_container(it, DBUS_TYPE_ARRAY,
 				DBUS_TYPE_OBJECT_PATH_AS_STRING, &array);
 
-	if (paths) {
-		while (*paths) {
-			const char *path = *paths++;
+	while (*ptr) {
+		const struct ril_slot_info *slot = *ptr++;
+		if (!selector || selector(slot)) {
+			const char *path = slot->path;
 			dbus_message_iter_append_basic(&array,
 						DBUS_TYPE_OBJECT_PATH, &path);
 		}
+	}
+
+	dbus_message_iter_close_container(it, &array);
+}
+
+static void ril_plugin_dbus_append_boolean_array(DBusMessageIter *it,
+	struct ril_plugin_dbus *dbus, ril_plugin_dbus_slot_select_fn value)
+{
+	DBusMessageIter array;
+	const struct ril_slot_info *const *ptr = dbus->plugin->slots;
+
+	dbus_message_iter_open_container(it, DBUS_TYPE_ARRAY,
+				DBUS_TYPE_BOOLEAN_AS_STRING, &array);
+
+	while (*ptr) {
+		const struct ril_slot_info *slot = *ptr++;
+		dbus_bool_t b = value(slot);
+
+		dbus_message_iter_append_basic(&array, DBUS_TYPE_BOOLEAN, &b);
 	}
 
 	dbus_message_iter_close_container(it, &array);
@@ -72,21 +110,21 @@ static void ril_plugin_dbus_append_path(DBusMessageIter *it, const char *path)
 }
 
 static void ril_plugin_dbus_message_append_path_array(DBusMessage *msg,
-								char **paths)
+	struct ril_plugin_dbus *dbus, ril_plugin_dbus_slot_select_fn fn)
 {
 	DBusMessageIter iter;
 
 	dbus_message_iter_init_append(msg, &iter);
-	ril_plugin_dbus_append_path_array(&iter, paths);
+	ril_plugin_dbus_append_path_array(&iter, dbus, fn);
 }
 
 static void ril_plugin_dbus_signal_path_array(struct ril_plugin_dbus *dbus,
-						const char *name, char **paths)
+			const char *name, ril_plugin_dbus_slot_select_fn fn)
 {
 	DBusMessage *signal = dbus_message_new_signal(RIL_DBUS_PATH,
 						RIL_DBUS_INTERFACE, name);
 
-	ril_plugin_dbus_message_append_path_array(signal, paths);
+	ril_plugin_dbus_message_append_path_array(signal, dbus, fn);
 	g_dbus_send_message(dbus->conn, signal);
 }
 
@@ -121,8 +159,8 @@ void ril_plugin_dbus_signal(struct ril_plugin_dbus *dbus, int mask)
 		}
 		if (mask & RIL_PLUGIN_SIGNAL_ENABLED_SLOTS) {
 			ril_plugin_dbus_signal_path_array(dbus,
-				RIL_DBUS_ENABLED_MODEM_CHANGED_SIGNAL,
-				dbus->plugin->enabled_slots);
+				RIL_DBUS_ENABLED_MODEMS_CHANGED_SIGNAL,
+				ril_plugin_dbus_enabled);
 		}
 		if (mask & RIL_PLUGIN_SIGNAL_VOICE_PATH) {
 			ril_plugin_dbus_signal_path(dbus,
@@ -137,66 +175,116 @@ void ril_plugin_dbus_signal(struct ril_plugin_dbus *dbus, int mask)
 	}
 }
 
-static DBusMessage *ril_plugin_dbus_reply_with_path_array(DBusConnection *conn,
-					DBusMessage *msg, char **paths)
+void ril_plugin_dbus_signal_sim(struct ril_plugin_dbus *dbus, int index,
+							gboolean present)
+{
+	dbus_bool_t value = present;
+	g_dbus_emit_signal(dbus->conn, RIL_DBUS_PATH, RIL_DBUS_INTERFACE,
+			RIL_DBUS_PRESENT_SIMS_CHANGED_SIGNAL,
+			DBUS_TYPE_INT32, &index,
+			DBUS_TYPE_BOOLEAN, &value,
+			DBUS_TYPE_INVALID);
+}
+
+static DBusMessage *ril_plugin_dbus_reply_with_path_array(DBusMessage *msg,
+	struct ril_plugin_dbus *dbus, ril_plugin_dbus_slot_select_fn fn)
 {
 	DBusMessage *reply = dbus_message_new_method_return(msg);
 
-	ril_plugin_dbus_message_append_path_array(reply, paths);
+	ril_plugin_dbus_message_append_path_array(reply, dbus, fn);
 	return reply;
+}
+
+static DBusMessage *ril_plugin_dbus_reply(DBusMessage *msg,
+		struct ril_plugin_dbus *dbus,
+		void (*append)(DBusMessageIter *, struct ril_plugin_dbus *))
+
+{
+	DBusMessage *reply = dbus_message_new_method_return(msg);
+	DBusMessageIter iter;
+
+	dbus_message_iter_init_append(reply, &iter);
+	append(&iter, dbus);
+	return reply;
+}
+
+static void ril_plugin_dbus_append_version(DBusMessageIter *it,
+						struct ril_plugin_dbus *dbus)
+{
+	dbus_int32_t version = RIL_DBUS_INTERFACE_VERSION;
+
+	dbus_message_iter_append_basic(it, DBUS_TYPE_INT32, &version);
+}
+
+static void ril_plugin_dbus_append_all(DBusMessageIter *it,
+						struct ril_plugin_dbus *dbus)
+{
+	ril_plugin_dbus_append_version(it, dbus);
+	ril_plugin_dbus_append_path_array(it, dbus, NULL);
+	ril_plugin_dbus_append_path_array(it, dbus, ril_plugin_dbus_enabled);
+	ril_plugin_dbus_append_imsi(it, dbus->plugin->default_data_imsi);
+	ril_plugin_dbus_append_imsi(it, dbus->plugin->default_voice_imsi);
+	ril_plugin_dbus_append_path(it, dbus->plugin->default_data_path);
+	ril_plugin_dbus_append_path(it, dbus->plugin->default_voice_path);
+}
+
+static void ril_plugin_dbus_append_all2(DBusMessageIter *it,
+						struct ril_plugin_dbus *dbus)
+{
+	ril_plugin_dbus_append_all(it, dbus);
+	ril_plugin_dbus_append_boolean_array(it, dbus, ril_plugin_dbus_present);
 }
 
 static DBusMessage *ril_plugin_dbus_get_all(DBusConnection *conn,
 						DBusMessage *msg, void *data)
 {
-	struct ril_plugin_dbus *dbus = data;
-	DBusMessage *reply = dbus_message_new_method_return(msg);
-	dbus_int32_t version = RIL_DBUS_INTERFACE_VERSION;
-	DBusMessageIter iter;
+	return ril_plugin_dbus_reply(msg, (struct ril_plugin_dbus *)data,
+						ril_plugin_dbus_append_all);
+}
 
-	dbus_message_iter_init_append(reply, &iter);
-	dbus_message_iter_append_basic(&iter, DBUS_TYPE_INT32, &version);
-	ril_plugin_dbus_append_path_array(&iter, dbus->plugin->available_slots);
-	ril_plugin_dbus_append_path_array(&iter, dbus->plugin->enabled_slots);
-	ril_plugin_dbus_append_imsi(&iter, dbus->plugin->default_data_imsi);
-	ril_plugin_dbus_append_imsi(&iter, dbus->plugin->default_voice_imsi);
-	ril_plugin_dbus_append_path(&iter, dbus->plugin->default_data_path);
-	ril_plugin_dbus_append_path(&iter, dbus->plugin->default_voice_path);
-	return reply;
+static DBusMessage *ril_plugin_dbus_get_all2(DBusConnection *conn,
+						DBusMessage *msg, void *data)
+{
+	return ril_plugin_dbus_reply(msg, (struct ril_plugin_dbus *)data,
+						ril_plugin_dbus_append_all2);
 }
 
 static DBusMessage *ril_plugin_dbus_get_interface_version(DBusConnection *conn,
 						DBusMessage *msg, void *data)
 {
-	DBusMessage *reply = dbus_message_new_method_return(msg);
-	dbus_int32_t version = RIL_DBUS_INTERFACE_VERSION;
-	DBusMessageIter iter;
-
-	dbus_message_iter_init_append(reply, &iter);
-	dbus_message_iter_append_basic(&iter, DBUS_TYPE_INT32, &version);
-	return reply;
+	return ril_plugin_dbus_reply(msg, (struct ril_plugin_dbus *)data,
+						ril_plugin_dbus_append_version);
 }
 
 static DBusMessage *ril_plugin_dbus_get_available_modems(DBusConnection *conn,
 						DBusMessage *msg, void *data)
 {
-	struct ril_plugin_dbus *dbus = data;
-
-	return ril_plugin_dbus_reply_with_path_array(conn, msg,
-					dbus->plugin->available_slots);
+	return ril_plugin_dbus_reply_with_path_array(msg,
+					(struct ril_plugin_dbus *)data, NULL);
 }
 
 static DBusMessage *ril_plugin_dbus_get_enabled_modems(DBusConnection *conn,
 						DBusMessage *msg, void *data)
 {
-	struct ril_plugin_dbus *dbus = data;
-
-	return ril_plugin_dbus_reply_with_path_array(conn, msg,
-					dbus->plugin->enabled_slots);
+	return ril_plugin_dbus_reply_with_path_array(msg,
+		(struct ril_plugin_dbus *)data, ril_plugin_dbus_enabled);
 }
 
-static DBusMessage *ril_plugin_dbus_reply_with_imsi(DBusConnection *conn,
-					DBusMessage *msg, const char *imsi)
+static void ril_plugin_dbus_append_present_sims(DBusMessageIter *it,
+						struct ril_plugin_dbus *dbus)
+{
+	ril_plugin_dbus_append_boolean_array(it, dbus, ril_plugin_dbus_present);
+}
+
+static DBusMessage *ril_plugin_dbus_get_present_sims(DBusConnection *conn,
+						DBusMessage *msg, void *data)
+{
+	return ril_plugin_dbus_reply(msg, (struct ril_plugin_dbus *)data,
+				ril_plugin_dbus_append_present_sims);
+}
+
+static DBusMessage *ril_plugin_dbus_reply_with_imsi(DBusMessage *msg,
+							const char *imsi)
 {
 	DBusMessage *reply = dbus_message_new_method_return(msg);
 	DBusMessageIter iter;
@@ -211,7 +299,7 @@ static DBusMessage *ril_plugin_dbus_get_default_data_sim(DBusConnection *conn,
 {
 	struct ril_plugin_dbus *dbus = data;
 
-	return ril_plugin_dbus_reply_with_imsi(conn, msg,
+	return ril_plugin_dbus_reply_with_imsi(msg,
 					dbus->plugin->default_data_imsi);
 }
 
@@ -220,12 +308,12 @@ static DBusMessage *ril_plugin_dbus_get_default_voice_sim(DBusConnection *conn,
 {
 	struct ril_plugin_dbus *dbus = data;
 
-	return ril_plugin_dbus_reply_with_imsi(conn, msg,
+	return ril_plugin_dbus_reply_with_imsi(msg,
 					dbus->plugin->default_voice_imsi);
 }
 
-static DBusMessage *ril_plugin_dbus_reply_with_path(DBusConnection *conn,
-					DBusMessage *msg, const char *path)
+static DBusMessage *ril_plugin_dbus_reply_with_path(DBusMessage *msg,
+							const char *path)
 {
 	DBusMessage *reply = dbus_message_new_method_return(msg);
 	DBusMessageIter iter;
@@ -240,7 +328,7 @@ static DBusMessage *ril_plugin_dbus_get_default_data_modem(DBusConnection *conn,
 {
 	struct ril_plugin_dbus *dbus = data;
 
-	return ril_plugin_dbus_reply_with_path(conn, msg,
+	return ril_plugin_dbus_reply_with_path(msg,
 					dbus->plugin->default_data_path);
 }
 
@@ -249,7 +337,7 @@ static DBusMessage *ril_plugin_dbus_get_default_voice_modem(DBusConnection *conn
 {
 	struct ril_plugin_dbus *dbus = data;
 
-	return ril_plugin_dbus_reply_with_path(conn, msg,
+	return ril_plugin_dbus_reply_with_path(msg,
 					dbus->plugin->default_voice_path);
 }
 
@@ -333,6 +421,16 @@ static const GDBusMethodTable ril_plugin_dbus_methods[] = {
 			{"defaultDataModem", "s" },
 			{"defaultVoiceModem" , "s"}),
 			ril_plugin_dbus_get_all) },
+	{ GDBUS_METHOD("GetAll2", NULL,
+			GDBUS_ARGS({"version", "i" },
+			{"availableModems", "ao" },
+			{"enabledModems", "ao" },
+			{"defaultDataSim", "s" },
+			{"defaultVoiceSim", "s" },
+			{"defaultDataModem", "s" },
+			{"defaultVoiceModem" , "s"},
+			{"presentSims" , "ab"}),
+			ril_plugin_dbus_get_all2) },
 	{ GDBUS_METHOD("GetInterfaceVersion",
 			NULL, GDBUS_ARGS({ "version", "i" }),
 			ril_plugin_dbus_get_interface_version) },
@@ -342,6 +440,9 @@ static const GDBusMethodTable ril_plugin_dbus_methods[] = {
 	{ GDBUS_METHOD("GetEnabledModems",
 			NULL, GDBUS_ARGS({ "modems", "ao" }),
 			ril_plugin_dbus_get_enabled_modems) },
+	{ GDBUS_METHOD("GetPresentSims",
+			NULL, GDBUS_ARGS({ "presentSims", "ab" }),
+			ril_plugin_dbus_get_present_sims) },
 	{ GDBUS_METHOD("GetDefaultDataSim",
 			NULL, GDBUS_ARGS({ "imsi", "s" }),
 			ril_plugin_dbus_get_default_data_sim) },
@@ -367,8 +468,11 @@ static const GDBusMethodTable ril_plugin_dbus_methods[] = {
 };
 
 static const GDBusSignalTable ril_plugin_dbus_signals[] = {
-	{ GDBUS_SIGNAL(RIL_DBUS_ENABLED_MODEM_CHANGED_SIGNAL,
+	{ GDBUS_SIGNAL(RIL_DBUS_ENABLED_MODEMS_CHANGED_SIGNAL,
 			GDBUS_ARGS({ "modems", "ao" })) },
+	{ GDBUS_SIGNAL(RIL_DBUS_PRESENT_SIMS_CHANGED_SIGNAL,
+			GDBUS_ARGS({"index", "i" },
+			{"present" , "b"})) },
 	{ GDBUS_SIGNAL(RIL_DBUS_DEFAULT_DATA_SIM_CHANGED_SIGNAL,
 			GDBUS_ARGS({ "imsi", "s" })) },
 	{ GDBUS_SIGNAL(RIL_DBUS_DEFAULT_VOICE_SIM_CHANGED_SIGNAL,
