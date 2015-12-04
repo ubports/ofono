b04013287dba23f238e3c8ed8628390e927ae0cf
Denis Kenzior <denkenz@gmail.com>
2015-12-03 18:00:41 -0600

    gril: Remove unused ril_cardstate_to_string

--- gril/grilutil.h
+++ gril/grilutil.h
@@ -32,7 +32,6 @@ extern "C" {
 #include "gril.h"
 
 int ril_protocol_string_to_ofono_protocol(gchar *protocol_str);
-const char *ril_cardstate_to_string(int card_state);
 const char *ril_error_to_string(int error);
 const char *ril_pinstate_to_string(int pin_state);
 const char *ril_radio_state_to_string(int radio_state);
