1a01727574fed5099221a035cf538804da8bfad2
Slava Monich <slava.monich@jolla.com>
2016-01-19 14:30:45 +0200

    Merge tag '63f6a75a61e7e07cc773929acb4c13acaf8a4b00' into merge-1.17
    Release 1.17
    
    Conflicts:
    	ofono/src/gprs.c
    	ofono/src/log.c
    	ofono/src/main.c
    	ofono/src/sim.c

--- src/log.c
+++ src/log.c
@@ -30,7 +30,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <syslog.h>
+#ifdef __GLIBC__
 #include <execinfo.h>
+#endif
 #include <dlfcn.h>
 
 #include "ofono.h"
@@ -113,6 +115,7 @@ void ofono_debug(const char *format, ...)
 	va_end(ap);
 }
 
+#ifdef __GLIBC__
 static void print_backtrace(unsigned int offset)
 {
 	void *frames[99];
@@ -240,6 +243,7 @@ static void signal_setup(sighandler_t handler)
 	sigaction(SIGABRT, &sa, NULL);
 	sigaction(SIGPIPE, &sa, NULL);
 }
+#endif
 
 extern struct ofono_debug_desc __start___debug[];
 extern struct ofono_debug_desc __stop___debug[];
@@ -311,8 +315,10 @@ int __ofono_log_init(const char *program, const char *debug,
 	if (detach == FALSE)
 		option |= LOG_PERROR;
 
+#ifdef __GLIBC__
 	if (backtrace == TRUE)
 		signal_setup(signal_handler);
+#endif
 
 	openlog(basename(program), option, LOG_DAEMON);
 
@@ -327,8 +333,10 @@ void __ofono_log_cleanup(ofono_bool_t backtrace)
 
 	closelog();
 
+#ifdef __GLIBC__
 	if (backtrace == TRUE)
 		signal_setup(SIG_DFL);
+#endif
 
 	g_strfreev(enabled);
 }
