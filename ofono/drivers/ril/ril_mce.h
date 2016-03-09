03b3ca77760a42dc525d2e621dca7e36446732c5
Slava Monich <slava.monich@jolla.com>
2016-03-09 13:40:28 +0200

    [ril] Refactored mce support
    Since there's only one mce, there's no need to have more than one mce client.
    Implemented the initial display state query.

--- ofono/drivers/ril/ril_mce.h
+++ ofono/drivers/ril/ril_mce.h
@@ -1,7 +1,7 @@
 /*
  *  oFono - Open Source Telephony - RIL-based devices
  *
- *  Copyright (C) 2015 Jolla Ltd.
+ *  Copyright (C) 2015-2016 Jolla Ltd.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
@@ -18,8 +18,27 @@
 
 #include "ril_types.h"
 
-struct ril_mce *ril_mce_new(GRilIoChannel *io);
-void ril_mce_free(struct ril_mce *mce);
+enum ril_mce_display_state {
+	RIL_MCE_DISPLAY_OFF,
+	RIL_MCE_DISPLAY_DIM,
+	RIL_MCE_DISPLAY_ON
+};
+
+struct ril_mce_priv;
+struct ril_mce {
+	GObject object;
+	struct ril_mce_priv *priv;
+	enum ril_mce_display_state display_state;
+};
+
+struct ril_mce *ril_mce_new(void);
+struct ril_mce *ril_mce_ref(struct ril_mce *mce);
+void ril_mce_unref(struct ril_mce *mce);
+
+typedef void (*ril_mce_cb_t)(struct ril_mce *mce, void *arg);
+gulong ril_mce_add_display_state_changed_handler(struct ril_mce *mce,
+					ril_mce_cb_t cb, void *arg);
+void ril_mce_remove_handler(struct ril_mce *mce, gulong id);
 
 #endif /* RIL_MCE_H */
 
