ef5acf0c8d941ae1a124ab141f8455f4d1d37d98
Nishanth V <nishanth.v@intel.com>
2016-03-31 09:51:42 +0530

    include: Add netmon changes

--- include/dbus.h
+++ include/dbus.h
@@ -60,6 +60,7 @@ extern "C" {
 #define OFONO_GNSS_POSR_AGENT_INTERFACE "org.ofono.PositioningRequestAgent"
 #define OFONO_HANDSFREE_INTERFACE OFONO_SERVICE ".Handsfree"
 #define OFONO_SIRI_INTERFACE OFONO_SERVICE ".Siri"
+#define OFONO_NETMON_INTERFACE OFONO_SERVICE ".NetworkMonitor"
 
 /* CDMA Interfaces */
 #define OFONO_CDMA_VOICECALL_MANAGER_INTERFACE "org.ofono.cdma.VoiceCallManager"
