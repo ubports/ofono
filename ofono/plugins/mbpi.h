4df72c9376e0de4d11803803ac04cf1f0268c7a4
Slava Monich <slava.monich@jolla.com>
2016-04-15 19:46:12 +0300

    [ofono] mbpi: Make default packet data protocol configurable. Contributes to JB#32750
    Note that according to TS 23.401, UE which is IPv6 and IPv4 capable
    should request IPv4v6.

--- ofono/plugins/mbpi.h
+++ ofono/plugins/mbpi.h
@@ -20,6 +20,9 @@
  */
 
 extern const char *mbpi_database;
+extern enum ofono_gprs_proto mbpi_default_internet_proto;
+extern enum ofono_gprs_proto mbpi_default_mms_proto;
+extern enum ofono_gprs_proto mbpi_default_proto;
 
 const char *mbpi_ap_type(enum ofono_gprs_context_type type);
 
