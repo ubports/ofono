56dd8f6f866e74a6b1a558c3942cd219cf9d1aff
Slava Monich <slava.monich@jolla.com>
2016-07-03 15:14:38 +0300

    [ril] Allow to specify default values in Settings section of ril_subscription.conf
    Those will become default for all modems but still can be overwritten in
    per-modem config sections.

---
+++ drivers/ril/ril_config.h
@@ -0,0 +1,41 @@
+/*
+ *  oFono - Open Source Telephony - RIL-based devices
+ *
+ *  Copyright (C) 2015-2016 Jolla Ltd.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ */
+
+#ifndef RIL_CONFIG_H
+#define RIL_CONFIG_H
+
+#include "ril_types.h"
+
+/* Utilities for parsing ril_subscription.conf */
+
+#define RILCONF_SETTINGS_GROUP      "Settings"
+
+char* ril_config_get_string(GKeyFile *file, const char *group, const char *key);
+gboolean ril_config_get_integer(GKeyFile *file, const char *group,
+					const char *key, int *value);
+gboolean ril_config_get_boolean(GKeyFile *file, const char *group,
+					const char *key, gboolean *value);
+gboolean ril_config_get_flag(GKeyFile *file, const char *group,
+					const char *key, int flag, int *flags);
+
+#endif /* RIL_CONFIG_H */
+
+/*
+ * Local Variables:
+ * mode: C
+ * c-basic-offset: 8
+ * indent-tabs-mode: t
+ * End:
+ */
