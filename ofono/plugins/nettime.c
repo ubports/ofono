1fe8701f1a168b64c69d42c2240577bfda40b649
Slava Monich <slava.monich@jolla.com>
2016-10-26 14:58:20 +0300

    [nettime] Handle NITZ information coming before MCC/MNC. Fixes MER#1680
    Due to the order in which events are delivered and network registration
    information requiring one extra query, NITZ information may be delivered
    to ofono before MCC/MNC are known.

--- ofono/plugins/nettime.c
+++ ofono/plugins/nettime.c
@@ -1,8 +1,7 @@
 /*
- *
  *  oFono - Open Source Telephony
  *
- *  Copyright (C) 2012-2015  Jolla Ltd.
+ *  Copyright (C) 2012-2016  Jolla Ltd.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
@@ -12,11 +11,6 @@
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
  */
 
 #ifdef HAVE_CONFIG_H
@@ -35,9 +29,34 @@
 
 #include "ofono.h"
 
+#define NTD_WAIT_TIMEOUT (500) /* ms */
+
+/*
+ * There are 3 distinct states here:
+ *
+ * 1. !valid
+ *
+ *     Initial state, no time/timezone information whatsoever.
+ *
+ * 2. valid && !mccmnc
+ *
+ *    Time/timezone information has been received from the network,
+ *    but no MCC and MNC yet.
+ *
+ * 3. valid && mccmnc
+ *
+ *    Time/timezone information is fully available.
+ *
+ */
 struct nt_data {
+	struct ofono_modem *modem;
+	struct ofono_netreg *netreg;
+	unsigned int netreg_watch_id;
+	unsigned int netreg_status_watch_id;
+	guint mccmnc_wait_id;
+
 	gboolean time_available;
-	gboolean time_pending;
+	gboolean valid;
 
 	time_t nw_time_utc;
 	time_t received;
@@ -47,27 +66,11 @@ struct nt_data {
 
 	char *mcc;
 	char *mnc;
-	char* path;
 	DBusConnection *conn;
 };
 
-static struct nt_data *nettime_new(const char *path)
-{
-	struct nt_data *ntd = g_new0(struct nt_data, 1);
-
-	ntd->path = g_strdup(path);
-	ntd->conn = dbus_connection_ref(ofono_dbus_get_connection());
-	return ntd;
-}
-
-static void nettime_free(struct nt_data *ntd)
-{
-	dbus_connection_unref(ntd->conn);
-	g_free(ntd->path);
-	g_free(ntd->mcc);
-	g_free(ntd->mnc);
-	g_free(ntd);
-}
+#define DBG_(ntd,fmt,args...) \
+	DBG("%s " fmt, ofono_modem_get_path((ntd)->modem), ##args)
 
 static gboolean nettime_encode_time_format(struct tm *tm,
 					const struct ofono_network_time *time)
@@ -110,7 +113,7 @@ static int nettime_fill_time_notification(DBusMessage *msg, struct nt_data *ntd)
 	dbus_message_iter_open_container(&iter,	DBUS_TYPE_ARRAY,
 						"{sv}",
 						&iter_array);
-	if (ntd->time_pending) {
+	if (ntd->valid && ntd->mcc && ntd->mnc) {
 		if (ntd->time_available) {
 			utc_long = (dbus_int64_t) ntd->nw_time_utc;
 			ofono_dbus_dict_append(&iter_array,
@@ -145,7 +148,7 @@ static int nettime_fill_time_notification(DBusMessage *msg, struct nt_data *ntd)
 					DBUS_TYPE_STRING,
 					&ntd->mnc);
 	} else {
-		DBG("fill_time_notification: time not available");
+		DBG_(ntd, "time not available");
 	}
 
 	dbus_message_iter_close_container(&iter, &iter_array);
@@ -177,23 +180,209 @@ static const GDBusSignalTable nettime_signals[] = {
 	{ }
 };
 
+static void nettime_send_signal(struct nt_data *ntd)
+{
+	DBusMessage *signal =
+		dbus_message_new_signal(ofono_modem_get_path(ntd->modem),
+			OFONO_NETWORK_TIME_INTERFACE, "NetworkTimeChanged");
+
+	DBG_(ntd, "");
+	nettime_fill_time_notification(signal, ntd);
+	g_dbus_send_message(ntd->conn, signal);
+}
+
+static void nettime_set_mcc_mnc(struct nt_data *ntd, const char *mcc,
+							const char *mnc)
+{
+	if (g_strcmp0(ntd->mcc, mcc)) {
+		g_free(ntd->mcc);
+		ntd->mcc = g_strdup(mcc);
+	}
+
+	if (g_strcmp0(ntd->mnc, mnc)) {
+		g_free(ntd->mnc);
+		ntd->mnc = g_strdup(mnc);
+	}
+
+	if (ntd->mcc && ntd->mnc) {
+		DBG_(ntd, "MCC: %s, MNC: %s", ntd->mcc, ntd->mnc);
+
+		if (ntd->mccmnc_wait_id) {
+			/* We have been waiting for MCC and MNC */
+			g_source_remove(ntd->mccmnc_wait_id);
+			ntd->mccmnc_wait_id = 0;
+			nettime_send_signal(ntd);
+		}
+	}
+}
+
+static void nettime_netreg_status_watch_cb(int status, int lac, int ci,
+			int tech, const char *mcc, const char *mnc,
+			void *userdata)
+{
+	nettime_set_mcc_mnc(userdata, mcc, mnc);
+}
+
+static void nettime_netreg_status_watch_done(void *userdata)
+{
+	struct nt_data *ntd = userdata;
+
+	DBG_(ntd, "");
+	ntd->netreg_status_watch_id = 0;
+}
+
+static void nettime_set_netreg(struct nt_data *ntd,
+						struct ofono_netreg *netreg)
+{
+	if (ntd->netreg != netreg) {
+		ntd->valid = FALSE;
+		ntd->netreg = netreg;
+		if (netreg) {
+			nettime_set_mcc_mnc(ntd,
+					ofono_netreg_get_mcc(netreg),
+					ofono_netreg_get_mnc(netreg));
+			ntd->netreg_status_watch_id =
+				__ofono_netreg_add_status_watch(netreg,
+					nettime_netreg_status_watch_cb, ntd,
+					nettime_netreg_status_watch_done);
+		} else {
+			g_free(ntd->mcc);
+			g_free(ntd->mnc);
+			ntd->mcc = NULL;
+			ntd->mnc = NULL;
+			ntd->netreg_status_watch_id = 0;
+		}
+	}
+}
+
+static void nettime_netreg_watch_cb(struct ofono_atom *atom,
+			enum ofono_atom_watch_condition cond, void *userdata)
+{
+	struct nt_data *ntd = userdata;
+
+	if (cond == OFONO_ATOM_WATCH_CONDITION_REGISTERED) {
+		DBG_(ntd, "registered");
+		nettime_set_netreg(ntd, __ofono_atom_get_data(atom));
+	} else if (cond == OFONO_ATOM_WATCH_CONDITION_UNREGISTERED) {
+		DBG_(ntd, "unregistered");
+		nettime_set_netreg(ntd, NULL);
+	}
+}
+
+static void nettime_netreg_watch_done(void *userdata)
+{
+	struct nt_data *ntd = userdata;
+
+	DBG_(ntd, "");
+	ntd->netreg_watch_id = 0;
+}
+
+static gboolean nettime_timeout_cb(gpointer data)
+{
+	struct nt_data *ntd = data;
+
+	DBG_(ntd, "timed out");
+	ntd->mccmnc_wait_id = 0;
+	ntd->valid = FALSE;
+	return G_SOURCE_REMOVE;
+}
+
+static struct nt_data *nettime_new(struct ofono_modem *modem)
+{
+	struct nt_data *ntd = g_new0(struct nt_data, 1);	
+	struct ofono_atom *netreg_atom = __ofono_modem_find_atom(modem,
+						OFONO_ATOM_TYPE_NETREG);
+
+	ntd->modem = modem;
+	ntd->conn = dbus_connection_ref(ofono_dbus_get_connection());
+	ntd->netreg_watch_id = __ofono_modem_add_atom_watch(modem,
+			OFONO_ATOM_TYPE_NETREG, nettime_netreg_watch_cb,
+			ntd, nettime_netreg_watch_done);
+
+	if (netreg_atom) {
+		nettime_set_netreg(ntd, __ofono_atom_get_data(netreg_atom));
+	}
+
+	return ntd;
+}
+
+static void nettime_free(struct nt_data *ntd)
+{
+	if (ntd->mccmnc_wait_id)
+		g_source_remove(ntd->mccmnc_wait_id);
+
+	if (ntd->netreg_status_watch_id)
+		__ofono_netreg_remove_status_watch(ntd->netreg,
+					ntd->netreg_status_watch_id);
+
+	if (ntd->netreg_watch_id)
+		__ofono_modem_remove_atom_watch(ntd->modem,
+					ntd->netreg_watch_id);
+
+	dbus_connection_unref(ntd->conn);
+	g_free(ntd->mcc);
+	g_free(ntd->mnc);
+	g_free(ntd);
+}
+
+static void nettime_info_received(struct ofono_nettime_context *context,
+					struct ofono_network_time *info)
+{
+	struct nt_data *ntd = context->data;
+	struct tm t;
+
+	if (!ntd)
+		return;
+
+	ntd->received = nettime_get_monotonic_time();
+	ntd->valid = TRUE;
+	ntd->dst = info->dst;
+	ntd->time_zone = info->utcoff;
+	ntd->time_available = nettime_encode_time_format(&t, info);
+	if (ntd->time_available) {
+		ntd->nw_time_utc = timegm(&t);
+	}
+
+	DBG_(ntd, "time: %04d-%02d-%02d %02d:%02d:%02d%c%02d:%02d (DST=%d)",
+		info->year, info->mon, info->mday, info->hour,
+		info->min, info->sec, info->utcoff > 0 ? '+' : '-',
+		abs(info->utcoff) / 3600, (abs(info->utcoff) % 3600) / 60,
+		info->dst);
+	DBG_(ntd, "UTC timestamp: %li, Received (monotonic time): %li",
+		ntd->nw_time_utc, ntd->received);
+
+	if (ntd->mcc && ntd->mnc) {
+		DBG_(ntd, "MCC: %s, MNC: %s", ntd->mcc, ntd->mnc);
+		nettime_send_signal(ntd);
+	} else {
+		DBG_(ntd, "no MCC/MNC yet");
+
+		if (ntd->mccmnc_wait_id)
+			g_source_remove(ntd->mccmnc_wait_id);
+
+		ntd->mccmnc_wait_id = g_timeout_add(NTD_WAIT_TIMEOUT,
+						nettime_timeout_cb, ntd);
+	}
+}
+
 static int nettime_probe(struct ofono_nettime_context *context)
 {
-	struct nt_data *ntd = nettime_new(ofono_modem_get_path(context->modem));
+	struct nt_data *ntd = nettime_new(context->modem);
+	const char *path = ofono_modem_get_path(context->modem);
 
-	DBG("Network time probe for modem: %p (%s)", context->modem, ntd->path);
-	if (g_dbus_register_interface(ntd->conn, ntd->path,
+	DBG("Network time probe for modem: %p (%s)", context->modem, path);
+	if (g_dbus_register_interface(ntd->conn, path,
 			OFONO_NETWORK_TIME_INTERFACE, nettime_methods,
 			nettime_signals, NULL, ntd, NULL)) {
 		context->data = ntd;
 		ofono_info("Registered interface %s, path %s",
-				OFONO_NETWORK_TIME_INTERFACE, ntd->path);
+				OFONO_NETWORK_TIME_INTERFACE, path);
 		ofono_modem_add_interface(context->modem,
 					OFONO_NETWORK_TIME_INTERFACE);
 		return 0;
 	} else {
 		ofono_error("Could not register interface %s, path %s",
-				OFONO_NETWORK_TIME_INTERFACE, ntd->path);
+				OFONO_NETWORK_TIME_INTERFACE, path);
 		nettime_free(ntd);
 		return 1;
 	}
@@ -202,75 +391,14 @@ static int nettime_probe(struct ofono_nettime_context *context)
 static void nettime_remove(struct ofono_nettime_context *context)
 {
 	struct nt_data *ntd = context->data;
+	const char *path = ofono_modem_get_path(context->modem);
 
-	DBG("Network time remove for modem: %p (%s)", context->modem,
-					ofono_modem_get_path(context->modem));
+	DBG("Network time remove for modem: %p (%s)", context->modem, path);
 	ofono_modem_remove_interface(context->modem,
 					OFONO_NETWORK_TIME_INTERFACE);
-	if (!g_dbus_unregister_interface(ntd->conn, ntd->path,
-					OFONO_NETWORK_TIME_INTERFACE)) {
-		ofono_error("Network time: could not unregister interface %s"
-			" for %s", OFONO_NETWORK_TIME_INTERFACE, ntd->path);
-	}
-
+	g_dbus_unregister_interface(ntd->conn, path,
+					OFONO_NETWORK_TIME_INTERFACE);
 	nettime_free(ntd);
-}
-
-static void nettime_send_signal(struct nt_data *ntd)
-{
-	DBusMessage *signal = dbus_message_new_signal(ntd->path,
-						OFONO_NETWORK_TIME_INTERFACE,
-							"NetworkTimeChanged");
-
-	nettime_fill_time_notification(signal, ntd);
-	g_dbus_send_message(ntd->conn, signal);
-}
-
-static void nettime_info_received(struct ofono_nettime_context *context,
-					struct ofono_network_time *info)
-{
-	struct nt_data *ntd = context->data;
-	struct ofono_netreg *netreg;
-	const char *mcc;
-	const char *mnc;
-	struct tm t;
-
-	if (!ntd)
-		return;
-
-	netreg = __ofono_atom_get_data(__ofono_modem_find_atom(
-				context->modem, OFONO_ATOM_TYPE_NETREG));
-	mcc = ofono_netreg_get_mcc(netreg);
-	mnc = ofono_netreg_get_mnc(netreg);
-
-	if (!mcc || !mnc) {
-		DBG("Incomplete network time received, ignoring");
-		return;
-	}
-
-	g_free(ntd->mcc);
-	g_free(ntd->mnc);
-	ntd->mcc = g_strdup(mcc);
-	ntd->mnc = g_strdup(mnc);
-	ntd->received = nettime_get_monotonic_time();
-	ntd->time_pending = TRUE;
-	ntd->dst = info->dst;
-	ntd->time_zone = info->utcoff;
-	ntd->time_available = nettime_encode_time_format(&t, info);
-	if (ntd->time_available) {
-		ntd->nw_time_utc = timegm(&t);
-	}
-
-	nettime_send_signal(ntd);
-	DBG("modem: %p (%s)", context->modem, ntd->path);
-	DBG("time: %04d-%02d-%02d %02d:%02d:%02d%c%02d:%02d (DST=%d)",
-		info->year, info->mon, info->mday, info->hour,
-		info->min, info->sec, info->utcoff > 0 ? '+' : '-',
-		abs(info->utcoff) / 3600, (abs(info->utcoff) % 3600) / 60,
-		info->dst);
-	DBG("UTC timestamp: %li, Received (monotonic time): %li",
-		ntd->nw_time_utc, ntd->received);
-	DBG("MCC: %s, MNC: %s", ntd->mcc, ntd->mnc);
 }
 
 static struct ofono_nettime_driver driver = {
