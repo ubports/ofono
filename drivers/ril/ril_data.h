d689a1156d767921bddfd1801b2a2594fc44c409
Slava Monich <slava.monich@jolla.com>
2017-05-30 18:50:53 +0300

    [ril] Notify rild when we don't need mobile data. JB#38689
    We were only telling rild when we need mobile data, but we should also
    tell it when we don't need it.

--- drivers/ril/ril_data.h
+++ drivers/ril/ril_data.h
@@ -1,7 +1,7 @@
 /*
  *  oFono - Open Source Telephony - RIL-based devices
  *
- *  Copyright (C) 2016 Jolla Ltd.
+ *  Copyright (C) 2016-2017 Jolla Ltd.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
@@ -56,8 +56,8 @@ enum ril_data_manager_flags {
 
 enum ril_data_allow_data_opt {
 	RIL_ALLOW_DATA_AUTO,
-	RIL_ALLOW_DATA_ON,
-	RIL_ALLOW_DATA_OFF
+	RIL_ALLOW_DATA_ENABLED,
+	RIL_ALLOW_DATA_DISABLED
 };
 
 enum ril_data_call_format {
