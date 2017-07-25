0870821715934c8cc81b69f868ed67576d0dcaef
Alexander Couzens <lynxis@fe80.eu>
2017-07-25 12:42:29 +0200

    add call-list helper to manage voice call lists
    Many drivers asks the modem for a complete call list of current calls.
    These list of calls can be feeded into call-list which parse the
    list and notify ofono for new calls.

--- drivers/common/call_list.h
+++ drivers/common/call_list.h
@@ -2,7 +2,7 @@
  *
  *  oFono - Open Source Telephony
  *
- *  Copyright (C) 2019 Alexander Couzens <lynxis@fe80.eu>
+ *  Copyright (C) 2017,2019 Alexander Couzens <lynxis@fe80.eu>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
@@ -24,8 +24,28 @@
 
 #include <glib.h>
 
+struct ofono_voicecall;
+struct ofono_phone_number;
+
 gint ofono_call_compare(gconstpointer a, gconstpointer b);
 gint ofono_call_compare_by_status(gconstpointer a, gconstpointer b);
 gint ofono_call_compare_by_id(gconstpointer a, gconstpointer b);
+
+/*
+ * Can be called by the driver in the dialing callback,
+ * when the new call id already known
+ */
+void ofono_call_list_dial_callback(struct ofono_voicecall *vc,
+				   GSList **call_list,
+				   const struct ofono_phone_number *ph,
+				   int call_id);
+
+/*
+ * Called with a list of known calls e.g. clcc.
+ * Call list will take ownership of all ofono call within the calls.
+ */
+void ofono_call_list_notify(struct ofono_voicecall *vc,
+			    GSList **call_list,
+			    GSList *calls);
 
 #endif /* __OFONO_DRIVER_COMMON_CALL_LIST */
