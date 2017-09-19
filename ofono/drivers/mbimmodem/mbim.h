bba23c3095c5c1704c17a61647205af0b39afea9
Denis Kenzior <denkenz@gmail.com>
2017-09-18 21:27:55 -0500

    mbim: Add skeleton mbim_device object

---
+++ ofono/drivers/mbimmodem/mbim.h
@@ -0,0 +1,39 @@
+/*
+ *
+ *  oFono - Open Source Telephony
+ *
+ *  Copyright (C) 2017  Intel Corporation. All rights reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+struct mbim_device;
+
+typedef void (*mbim_device_debug_func_t) (const char *str, void *user_data);
+typedef void (*mbim_device_disconnect_func_t) (void *user_data);
+typedef void (*mbim_device_destroy_func_t) (void *user_data);
+
+struct mbim_device *mbim_device_new(int fd);
+bool mbim_device_set_close_on_unref(struct mbim_device *device, bool do_close);
+struct mbim_device *mbim_device_ref(struct mbim_device *device);
+void mbim_device_unref(struct mbim_device *device);
+
+bool mbim_device_set_debug(struct mbim_device *device,
+				mbim_device_debug_func_t func, void *user_data,
+				mbim_device_destroy_func_t destroy);
+bool mbim_device_set_disconnect_handler(struct mbim_device *device,
+					mbim_device_disconnect_func_t function,
+					void *user_data,
+					mbim_device_destroy_func_t destroy);
