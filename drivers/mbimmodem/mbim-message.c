76811bb32690f9a28b4f483206317b2dc35eb602
Denis Kenzior <denkenz@gmail.com>
2017-09-29 11:41:54 -0500

    mbim: Add basic message builder support

--- drivers/mbimmodem/mbim-message.c
+++ drivers/mbimmodem/mbim-message.c
@@ -36,6 +36,7 @@
 
 static const char CONTAINER_TYPE_ARRAY	= 'a';
 static const char CONTAINER_TYPE_STRUCT	= 'r';
+static const char *simple_types = "syqu";
 
 struct mbim_message {
 	int ref_count;
@@ -91,6 +92,20 @@ static int get_alignment(const char type)
 	case 's':
 		return 4;
 	case 'a':
+		return 4;
+	default:
+		return 0;
+	}
+}
+
+static int get_basic_size(const char type)
+{
+	switch (type) {
+	case 'y':
+		return 1;
+	case 'q':
+		return 2;
+	case 'u':
 		return 4;
 	default:
 		return 0;
@@ -390,7 +405,6 @@ static bool _iter_enter_struct(struct mbim_message_iter *iter,
 static bool message_iter_next_entry_valist(struct mbim_message_iter *orig,
 						va_list args)
 {
-	static const char *simple_types = "syqu";
 	struct mbim_message_iter *iter = orig;
 	const char *signature = orig->sig_start + orig->sig_pos;
 	const char *end;
@@ -618,4 +632,253 @@ bool mbim_message_get_arguments(struct mbim_message *message,
 	va_end(args);
 
 	return result;
+}
+
+struct container {
+	void *sbuf;		/* static buffer */
+	size_t sbuf_size;
+	size_t sbuf_pos;
+	void *dbuf;		/* data buffer */
+	size_t dbuf_size;
+	size_t dbuf_pos;
+	void *obuf;		/* offset buffer */
+	size_t obuf_size;
+	size_t obuf_pos;
+	char container_type;
+	char signature[256];
+	uint8_t sigindex;
+	uint32_t base_offset;
+};
+
+static void container_update_offsets(struct container *container)
+{
+	size_t i;
+
+	if (!container->obuf)
+		return;
+
+	for (i = 0; i < container->obuf_pos; i += 4) {
+		uint32_t sbuf_offset = l_get_u32(container->obuf + i);
+		uint32_t dbuf_offset = l_get_u32(container->sbuf + sbuf_offset);
+
+		dbuf_offset += container->sbuf_pos - container->base_offset;
+		l_put_le32(dbuf_offset, container->sbuf + sbuf_offset);
+	}
+
+	l_free(container->obuf);
+	container->obuf = NULL;
+	container->obuf_pos = 0;
+	container->obuf_size = 0;
+}
+
+struct mbim_message_builder {
+	struct mbim_message *message;
+	struct container stack[3];
+	uint32_t index;
+};
+
+static inline size_t grow_buf(void **buf, size_t *buf_size, size_t *pos,
+				size_t len, unsigned int alignment)
+{
+	size_t size = align_len(*pos, alignment);
+
+	if (size + len > *buf_size) {
+		*buf = l_realloc(*buf, size + len);
+		*buf_size = size + len;
+	}
+
+	if (size - *pos > 0)
+		memset(*buf + *pos, 0, size - *pos);
+
+	*pos = size + len;
+	return size;
+}
+
+#define GROW_SBUF(c, len, alignment) \
+	grow_buf(&c->sbuf, &c->sbuf_size, &c->sbuf_pos, \
+			len, alignment)
+
+#define GROW_DBUF(c, len, alignment) \
+	grow_buf(&c->dbuf, &c->dbuf_size, &c->dbuf_pos, \
+			len, alignment)
+
+#define GROW_OBUF(c) \
+	grow_buf(&c->obuf, &c->obuf_size, &c->obuf_pos, 4, 4)
+
+static void add_offset_and_length(struct container *container,
+					uint32_t offset, uint32_t len)
+{
+	size_t start;
+	/*
+	 * note the relative offset in the data buffer.  Store it in native
+	 * endian order for now.  It will be fixed up later once we finalize
+	 * the structure
+	 */
+	start = GROW_SBUF(container, 8, 4);
+	l_put_u32(offset, container->sbuf + start);
+	l_put_le32(len, container->sbuf + start + 4);
+
+	/* Make a note in offset buffer to update the offset at this position */
+	offset = start;
+	start = GROW_OBUF(container);
+	l_put_u32(offset, container->obuf + start);
+}
+
+struct mbim_message_builder *mbim_message_builder_new(struct mbim_message *msg)
+{
+	struct mbim_message_builder *ret;
+	struct mbim_message_header *hdr;
+	uint32_t type;
+	struct container *container;
+
+	if (unlikely(!msg))
+		return NULL;
+
+	if (msg->sealed)
+		return NULL;
+
+	hdr = (struct mbim_message_header *) msg->header;
+	type = L_LE32_TO_CPU(hdr->type);
+
+	ret = l_new(struct mbim_message_builder, 1);
+	ret->message = mbim_message_ref(msg);
+
+	/* Reserve space in the static buffer for UUID, CID, Status, etc */
+	container = &ret->stack[ret->index];
+	container->base_offset = _mbim_information_buffer_offset(type);
+	GROW_SBUF(container, container->base_offset, 0);
+
+	return ret;
+}
+
+void mbim_message_builder_free(struct mbim_message_builder *builder)
+{
+	uint32_t i;
+
+	if (unlikely(!builder))
+		return;
+
+	mbim_message_unref(builder->message);
+
+	for (i = 0; i <= builder->index; i++) {
+		l_free(builder->stack[i].sbuf);
+		l_free(builder->stack[i].dbuf);
+		l_free(builder->stack[i].obuf);
+	}
+
+	l_free(builder);
+}
+
+bool mbim_message_builder_append_basic(struct mbim_message_builder *builder,
+					char type, const void *value)
+{
+	struct container *container = &builder->stack[builder->index];
+	size_t start;
+	unsigned int alignment;
+	size_t len;
+	uint16_t *utf16;
+
+	if (unlikely(!builder))
+		return false;
+
+	if (unlikely(!strchr(simple_types, type)))
+		return false;
+
+	alignment = get_alignment(type);
+	if (!alignment)
+		return false;
+
+	if (builder->index > 0 &&
+			container->signature[container->sigindex] != type)
+		return false;
+
+	len = get_basic_size(type);
+
+	if (len) {
+		start = GROW_SBUF(container, len, alignment);
+		memcpy(container->sbuf + start, value, len);
+		goto done;
+	}
+
+	/* Null string? */
+	if (!value) {
+		start = GROW_SBUF(container, 8, 4);
+		l_put_le32(0, container->sbuf + start);
+		l_put_le32(0, container->sbuf + start + 4);
+		goto done;
+	}
+
+	utf16 = l_utf8_to_utf16(value, &len);
+	if (!utf16)
+		return false;
+
+	/* Strings are in UTF16-LE, so convert if needed */
+	if (L_CPU_TO_LE16(0x8000) != 0x8000) {
+		size_t i;
+
+		for (i = 0; i < len - 2; i += 2)
+			utf16[i] = __builtin_bswap16(utf16[i]);
+	}
+
+	/*
+	 * First grow the data buffer.
+	 * MBIM v1.0-errata1, Section 10.3:
+	 * "If the size of the payload in the variable field is not a multiple
+	 * of 4 bytes, the field shall be padded up to the next 4 byte multiple.
+	 * This shall be true even for the last payload in DataBuffer."
+	 */
+	start = GROW_DBUF(container, len - 2, 4);
+	memcpy(container->dbuf + start, utf16, len - 2);
+	l_free(utf16);
+
+	add_offset_and_length(container, start, len - 2);
+
+done:
+	if (container->container_type != CONTAINER_TYPE_ARRAY)
+		container->sigindex += 1;
+
+	return true;
+}
+
+struct mbim_message *mbim_message_builder_finalize(
+					struct mbim_message_builder *builder)
+{
+	struct container *root;
+	struct mbim_message_header *hdr;
+
+	if (unlikely(!builder))
+		return NULL;
+
+	if (builder->index != 0)
+		return NULL;
+
+	root = &builder->stack[0];
+	GROW_DBUF(root, 0, 4);
+	container_update_offsets(root);
+
+	memcpy(root->sbuf, builder->message->uuid, 16);
+	l_put_le32(builder->message->cid, root->sbuf + 16);
+	l_put_le32(builder->message->status, root->sbuf + 20);
+
+	builder->message->info_buf_len = root->dbuf_pos + root->sbuf_pos -
+						root->base_offset;
+	l_put_le32(builder->message->info_buf_len,
+					root->sbuf + root->base_offset - 4);
+
+	builder->message->n_frags = 2;
+	builder->message->frags = l_new(struct iovec, 2);
+	builder->message->frags[0].iov_base = root->sbuf;
+	builder->message->frags[0].iov_len = root->sbuf_pos;
+	builder->message->frags[1].iov_base = root->dbuf;
+	builder->message->frags[1].iov_len = root->dbuf_pos;
+
+	root->sbuf = NULL;
+	root->dbuf = NULL;
+
+	hdr = (struct mbim_message_header *) builder->message->header;
+	hdr->len = L_CPU_TO_LE32(HEADER_SIZE + root->dbuf_pos + root->sbuf_pos);
+
+	builder->message->sealed = true;
+
+	return builder->message;
 }
