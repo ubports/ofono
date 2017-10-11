2396ead477df2df4a89958502cfd42e19322785b
James Prestwood <james.prestwood@linux.intel.com>
2017-10-11 11:57:42 -0700

    sim-auth: prep simauth/dbus headers
    Added new dbus interfaces for SimAuth module as well as
    function prototype definitions to simauth header.
    
    org.ofono.SimAuthentication:
       Interface to hold the auth object to type mapping property
    
    org.ofono.USimApplication:
       Application with USim functionality (GSM/UMTS auth)
    
    org.ofono.ISimApplication:
       Application with ISim functionality (IMS auth)

--- ofono/include/sim-auth.h
+++ ofono/include/sim-auth.h
@@ -26,6 +26,8 @@
 extern "C" {
 #endif
 
+#include <stdint.h>
+
 #include <ofono/types.h>
 
 struct ofono_sim_auth;
@@ -34,6 +36,15 @@ typedef void (*ofono_sim_list_apps_cb_t)(const struct ofono_error *error,
 					const unsigned char *dataobj,
 					int len, void *data);
 
+typedef void (*ofono_sim_open_channel_cb_t)(const struct ofono_error *error,
+		int session_id, void *data);
+
+typedef void (*ofono_sim_close_channel_cb_t)(const struct ofono_error *error,
+		void *data);
+
+typedef void (*ofono_logical_access_cb_t)(const struct ofono_error *error,
+		const uint8_t *resp, uint16_t len, void *data);
+
 struct ofono_sim_auth_driver {
 	const char *name;
 	int (*probe)(struct ofono_sim_auth *sa, unsigned int vendor,
@@ -41,7 +52,14 @@ struct ofono_sim_auth_driver {
 	void (*remove)(struct ofono_sim_auth *sa);
 
 	void (*list_apps)(struct ofono_sim_auth *sa,
-				ofono_sim_list_apps_cb_t cb, void *data);
+			ofono_sim_list_apps_cb_t cb, void *data);
+	void (*open_channel)(struct ofono_sim_auth *sa, const uint8_t *aid,
+			ofono_sim_open_channel_cb_t cb, void *data);
+	void (*close_channel)(struct ofono_sim_auth *sa, int session_id,
+			ofono_sim_close_channel_cb_t cb, void *data);
+	void (*logical_access)(struct ofono_sim_auth *sa,
+			int session_id, const uint8_t *pdu, uint16_t len,
+			ofono_logical_access_cb_t cb, void *data);
 };
 
 int ofono_sim_auth_driver_register(const struct ofono_sim_auth_driver *d);
