6105412d093653f7ea85eb92f8abcf5fde9b0bcd
James Prestwood <james.prestwood@linux.intel.com>
2017-10-12 09:28:10 -0700

    xmm7xxx: add sim-auth driver to xmm7xxx plugin

--- plugins/xmm7xxx.c
+++ plugins/xmm7xxx.c
@@ -49,6 +49,7 @@
 #include <ofono/stk.h>
 #include <ofono/lte.h>
 #include <ofono/ims.h>
+#include <ofono/sim-auth.h>
 
 #include <drivers/atmodem/atutil.h>
 #include <drivers/atmodem/vendor.h>
@@ -322,6 +323,7 @@ static void xmm7xxx_post_sim(struct ofono_modem *modem)
 
 	ofono_lte_create(modem, "atmodem", data->chat);
 	ofono_radio_settings_create(modem, 0, "xmm7modem", data->chat);
+	ofono_sim_auth_create(modem, 0, "atmodem", data->chat);
 }
 
 static void xmm7xxx_post_online(struct ofono_modem *modem)
