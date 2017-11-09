020dc3020ec3e7de1855fa5fe552736628d75577
James Prestwood <james.prestwood@linux.intel.com>
2017-11-09 09:52:15 -0800

    simauth: remove driver API definitions from simauth include

--- ofono/include/sim-auth.h
+++ ofono/include/sim-auth.h
@@ -32,48 +32,8 @@ extern "C" {
 
 struct ofono_sim_auth;
 
-typedef void (*ofono_sim_list_apps_cb_t)(const struct ofono_error *error,
-					const unsigned char *dataobj,
-					int len, void *data);
-
-typedef void (*ofono_sim_open_channel_cb_t)(const struct ofono_error *error,
-		int session_id, void *data);
-
-typedef void (*ofono_sim_close_channel_cb_t)(const struct ofono_error *error,
-		void *data);
-
-typedef void (*ofono_logical_access_cb_t)(const struct ofono_error *error,
-		const uint8_t *resp, uint16_t len, void *data);
-
-struct ofono_sim_auth_driver {
-	const char *name;
-	int (*probe)(struct ofono_sim_auth *sa, unsigned int vendor,
-			void *data);
-	void (*remove)(struct ofono_sim_auth *sa);
-
-	void (*list_apps)(struct ofono_sim_auth *sa,
-			ofono_sim_list_apps_cb_t cb, void *data);
-	void (*open_channel)(struct ofono_sim_auth *sa, const uint8_t *aid,
-			ofono_sim_open_channel_cb_t cb, void *data);
-	void (*close_channel)(struct ofono_sim_auth *sa, int session_id,
-			ofono_sim_close_channel_cb_t cb, void *data);
-	void (*logical_access)(struct ofono_sim_auth *sa,
-			int session_id, const uint8_t *pdu, uint16_t len,
-			ofono_logical_access_cb_t cb, void *data);
-};
-
-int ofono_sim_auth_driver_register(const struct ofono_sim_auth_driver *d);
-void ofono_sim_auth_driver_unregister(const struct ofono_sim_auth_driver *d);
-
-struct ofono_sim_auth *ofono_sim_auth_create(struct ofono_modem *modem,
-						unsigned int vendor,
-						const char *driver, void *data);
-
-void ofono_sim_auth_register(struct ofono_sim_auth *sa);
+struct ofono_sim_auth *ofono_sim_auth_create(struct ofono_modem *modem);
 void ofono_sim_auth_remove(struct ofono_sim_auth *sa);
-
-void ofono_sim_auth_set_data(struct ofono_sim_auth *sa, void *data);
-void *ofono_sim_auth_get_data(struct ofono_sim_auth *sa);
 
 #ifdef __cplusplus
 }
