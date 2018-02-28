f597119845c25169b2a58fd3ad13eaa69d1bcda4
Denis Kenzior <denkenz@gmail.com>
2018-02-28 11:08:29 -0600

    unit: Use sim_app_record_free to avoid memleaks

--- ofono/unit/test-simutil.c
+++ ofono/unit/test-simutil.c
@@ -500,6 +500,7 @@ static void test_application_entry_decode(void)
 	g_assert(app[1]->label != NULL);
 	g_assert(!strcmp(app[1]->label, "MIDPfiles"));
 
+	g_slist_free_full(entries, (GDestroyNotify) sim_app_record_free);
 	g_free(ef_dir);
 	g_slist_free_full(entries, (GDestroyNotify) sim_app_record_free);
 }
