d55872a0d6151d17813d36b318056e27cc7ea2f8
Slava Monich <slava.monich@jolla.com>
2018-06-29 16:57:40 +0300

    modem: Implement ofono_modem_get_voicecall

--- src/modem.c
+++ src/modem.c
@@ -195,6 +195,11 @@ struct ofono_gprs *ofono_modem_get_gprs(struct ofono_modem *modem)
 	return __ofono_atom_find(OFONO_ATOM_TYPE_GPRS, modem);
 }
 
+struct ofono_voicecall *ofono_modem_get_voicecall(struct ofono_modem *modem)
+{
+	return __ofono_atom_find(OFONO_ATOM_TYPE_VOICECALL, modem);
+}
+
 struct ofono_atom *__ofono_modem_add_atom(struct ofono_modem *modem,
 					enum ofono_atom_type type,
 					void (*destruct)(struct ofono_atom *),
