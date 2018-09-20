59162e43632aa571b2466c0883b6cee17c402965
Giacinto Cifelli <gciofono@gmail.com>
2018-09-20 06:39:41 +0200

    tools: remove calls to g_thread_init

--- tools/huawei-audio.c
+++ tools/huawei-audio.c
@@ -775,11 +775,6 @@ int main(int argc, char **argv)
 	guint watch;
 	struct sigaction sa;
 
-#ifdef NEED_THREADS
-	if (g_thread_supported() == FALSE)
-		g_thread_init(NULL);
-#endif
-
 	context = g_option_context_new(NULL);
 	g_option_context_add_main_entries(context, options, NULL);
 
