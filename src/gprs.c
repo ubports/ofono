91253a1adeed452371372eec7e338ed640ada6cd
Denis Kenzior <denkenz@gmail.com>
2019-04-29 19:25:57 -0500

    gprs: Remove unneeded if statement
    if (ctx->message_proxy) always resolves to TRUE

--- src/gprs.c
+++ src/gprs.c
@@ -719,9 +719,7 @@ static void pri_update_mms_context_settings(struct pri_context *ctx)
 	struct ofono_gprs_context *gc = ctx->context_driver;
 	struct context_settings *settings = gc->settings;
 
-	if (ctx->message_proxy)
-		settings->ipv4->proxy = g_strdup(ctx->message_proxy);
-
+	settings->ipv4->proxy = g_strdup(ctx->message_proxy);
 	pri_parse_proxy(ctx, ctx->message_proxy);
 
 	DBG("proxy %s port %u", ctx->proxy_host, ctx->proxy_port);
