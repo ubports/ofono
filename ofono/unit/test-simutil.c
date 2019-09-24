c04b14c49a11ee97813dcbe012ef236e60c3f14a
Slava Monich <slava.monich@jolla.com>
2019-09-24 17:40:46 +0300

    [unit] Fixed memory leak in test-simutil
    ==7578==    at 0x4C2AB80: malloc
    ==7578==    by 0x4E856D0: g_malloc
    ==7578==    by 0x4E9B2ED: g_slice_alloc
    ==7578==    by 0x4E9C0C5: g_slist_prepend
    ==7578==    by 0x40EC77: sim_parse_app_template_entries (simutil.c:1604)
    ==7578==    by 0x405AF6: test_application_entry_decode (test-simutil.c:487)
    ==7578==    by 0x4EA3D05: g_test_run_suite_internal
    ==7578==    by 0x4EA405A: g_test_run_suite
    ==7578==    by 0x4EA4090: g_test_run
    ==7578==    by 0x40714E: main (test-simutil.c:655)

--- ofono/unit/test-simutil.c
+++ ofono/unit/test-simutil.c
@@ -501,6 +501,7 @@ static void test_application_entry_decode(void)
 	g_assert(!strcmp(app[1]->label, "MIDPfiles"));
 
 	g_free(ef_dir);
+	g_slist_free_full(entries, (GDestroyNotify) sim_app_record_free);
 }
 
 static void test_get_3g_path(void)
