53929f9f1a6979c386586e558dee4b2bdc398c48
Denis Grigorev <d.grigorev@omprussia.ru>
2020-04-30 20:58:06 +0300

    [ril] Fix RilCellInfo object leak. JB#49798
    On some devices RIL requires to reset the modem while switching radio
    capabilities. During this procedure all associated objects should be
    disposed and replaced by new instances. This patch fixes a memory
    leak through RilCellInfo object.

--- ofono/drivers/ril/ril_plugin.c
+++ ofono/drivers/ril/ril_plugin.c
@@ -384,6 +384,7 @@ static void ril_plugin_shutdown_slot(ril_slot *slot, gboolean kill_io)
 		}
 
 		if (slot->cell_info) {
+			sailfish_manager_set_cell_info(slot->handle, NULL);
 			sailfish_cell_info_unref(slot->cell_info);
 			slot->cell_info = NULL;
 		}
@@ -1041,13 +1042,13 @@ static void ril_plugin_slot_connected(ril_slot *slot)
 				slot->path, slot->config.techs, slot->imei,
 				slot->imeisv, ril_plugin_sim_state(slot),
 				slot->slot_flags);
-		sailfish_manager_set_cell_info(slot->handle, slot->cell_info);
 		grilio_channel_set_enabled(slot->io, slot->handle->enabled);
 
 		/* Check if this was the last slot we were waiting for */
 		ril_plugin_check_if_started(plugin);
 	}
 
+	sailfish_manager_set_cell_info(slot->handle, slot->cell_info);
 	ril_plugin_check_modem(slot);
 	ril_plugin_check_ready(slot);
 }
