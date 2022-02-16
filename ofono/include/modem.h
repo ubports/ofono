78e83f877c4142f39fd212fc643c45fc1e6f12ad
Slava Monich <slava.monich@jolla.com>
2022-02-16 17:05:03 +0200

    [ofono] Added API for querying IMEI from the modem. JB#56657
    ofono_devinfo_get_serial(ofono_modem_get_devinfo(modem))

--- ofono/include/modem.h
+++ ofono/include/modem.h
@@ -3,7 +3,7 @@
  *  oFono - Open Source Telephony
  *
  *  Copyright (C) 2008-2011  Intel Corporation. All rights reserved.
- *  Copyright (C) 2015-2021  Jolla Ltd.
+ *  Copyright (C) 2015-2022  Jolla Ltd.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
@@ -29,6 +29,7 @@ extern "C" {
 
 #include <ofono/types.h>
 
+struct ofono_devinfo;
 struct ofono_modem;
 struct ofono_gprs;
 struct ofono_sim;
@@ -92,7 +93,9 @@ struct ofono_gprs *ofono_modem_get_gprs(struct ofono_modem *modem);
 struct ofono_voicecall *ofono_modem_get_voicecall(struct ofono_modem *modem);
 struct ofono_netreg *ofono_modem_get_netreg(struct ofono_modem *modem);
 struct ofono_radio_settings *ofono_modem_get_radio_settings
-			(struct ofono_modem *modem); /* Since mer/1.24+git2 */
+			(struct ofono_modem *modem); /* Since 1.24+git2 */
+struct ofono_devinfo *ofono_modem_get_devinfo
+			(struct ofono_modem *modem); /* Since 1.28+git4 */
 
 void ofono_modem_set_data(struct ofono_modem *modem, void *data);
 void *ofono_modem_get_data(struct ofono_modem *modem);
